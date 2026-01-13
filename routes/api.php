86b10fdde26d56b9669487f222a86814254842ce
etiusk03 <angel.gonzalez.castillo2003@gmail.com>
2026-01-13 13:46:34 -0400

    Peque cambios

--- routes/api.php
+++ routes/api.php
@@ -3,51 +3,31 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Api\Admin\DireccionController;
+use App\Http\Controllers\Api\Admin\AuditoriaController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// --- 1. Rutas Públicas (Cualquiera entra) ---
+// ----- Rutas Públicas (Cualquiera entra) ---
 Route::prefix('autenticacion')->group(function () {
     Route::post('iniciar-sesion', [AuthController::class, 'login']);
-    // Aquí iría Route::post('registro', ...) para usuarios públicos en el futuro
     Route::post('registrar', [AuthController::class, 'register']);
 });
 
 // --- 2. Rutas Protegidas (Necesitas Token "Bearer") ---
+// Para ingresar a estas rutas primero tienen que iniciar sesion, 
+// luego les dara un token: xxxxxxxxxxxxxxxxxxx
+// eso lo copian y lo pegan en Auth
 Route::middleware('auth:sanctum')->group(function () {
-    
+
     // Ver mis propios datos
     Route::get('autenticacion/perfil', [AuthController::class, 'perfil']);
 
-    // --- ZONA ADMIN (Solo Admin) ---
-    Route::prefix('admin')->middleware('role:admin')->group(function () {
-        Route::get('dashboard', function () {
-            return response()->json(['mensaje' => 'Hola Admin, tienes control total.']);
-        });
-        // Aquí iría: crear roles, borrar usuarios, etc.
-    });
+    Route::resource('direcciones', DireccionController::class)
+        ->parameters(['direcciones' => 'direccion']);
 
-    // --- ZONA TÉCNICA (Técnicos Y Supervisores) ---
-    // Usamos pipe '|' para decir "uno U otro".
-    Route::prefix('tecnica')->middleware('role:tecnico|supervisor|admin')->group(function () {
-        Route::get('ordenes-trabajo', function () {
-            return response()->json(['mensaje' => 'Listado de órdenes de trabajo (Visible para Técnico y Supervisor)']);
-        });
-    });
-
-    // --- ZONA SUPERVISIÓN (Solo Supervisores) ---
-    Route::prefix('supervision')->middleware('role:supervisor|admin')->group(function () {
-        Route::get('auditoria', function () {
-            return response()->json(['mensaje' => 'Auditoría de calidad (Solo Supervisor)']);
-        });
-    });
-
-    // --- ZONA REPORTES (Solo Reporter) ---
-    Route::prefix('reportes')->middleware('role:reporter|admin')->group(function () {
-        Route::get('mensuales', function () {
-            return response()->json(['mensaje' => 'Descarga de excel/pdf (Solo Reporter)']);
-        });
-    });
+    Route::resource('auditorias', AuditoriaController::class)
+        ->parameters(['auditorias' => 'auditoria']);
 });
